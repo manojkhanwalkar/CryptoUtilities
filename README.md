f5e271462d1f102c621b2a2d30cd8edb18f23dcc
Manoj <manoj.khanwalkar@gmail.com>
2020-12-29 09:36:44 -0500

    HMAC generator

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # **Crypto Utilities**
 
-A set of functions that I have found useful in dealing with X509 certificates , private keys in DER and PEM formats etc.
+A set of functions that I have found useful in dealing with X509 certificates , private keys in DER and PEM formats etc. for application level encryption.
 The utilities cover RSA/EC asymmetric keys as well as AES symmetric key. 
+
+* JSONUtil - Wrapper over the Jackson library to convert a java object to json string and vice versa. 
+* IdGenerator - Wrapper over secure random and uuid to generate int and string id's. 
+* HMACUtil - Creates a HMAC string. HMAC is used to both authenticate (both sides have access to the secret key) as well as for message integrity. (The HMAC is calculated over the message).
 
 Work in Progress 
--- src/main/java/util/HMACUtil.java
+++ src/main/java/util/HMACUtil.java
@@ -2,6 +2,11 @@ package util;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
@@ -13,13 +18,34 @@ public class HMACUtil {
 
 
     public static String calculateHMAC(String data, String key)
+    {
+        return calculateHMAC(data.getBytes(),key.getBytes());
+
+    }
+
+    public static byte[] serialize(Object obj) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(bos);
+        os.writeObject(obj);
+        return bos.toByteArray();
+    }
+
+
+    public static String calculateHMAC(Object data, Object key) throws IOException
+    {
+
+        return calculateHMAC(serialize(data),serialize(key));
+
+    }
+
+    public static String calculateHMAC(byte[] data, byte[] key)
 
     {
         try {
-            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), HMAC_SHA512);
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key, HMAC_SHA512);
             Mac mac = Mac.getInstance(HMAC_SHA512);
             mac.init(secretKeySpec);
-            return Base64.getEncoder().encodeToString(mac.doFinal(data.getBytes()));
+            return Base64.getEncoder().encodeToString(mac.doFinal(data));
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (InvalidKeyException e) {
@@ -36,6 +62,21 @@ public class HMACUtil {
         return hmac.equals(calculateHMAC(data,key));
 
     }
+
+    public static boolean isValid(String hmac , Object data , Object key) throws IOException
+    {
+
+        return hmac.equals(calculateHMAC(data,key));
+
+    }
+
+    public static boolean isValid(String hmac , byte[] data , byte[] key)
+    {
+
+        return hmac.equals(calculateHMAC(data,key));
+
+    }
+
 
     public static void main(String[] args) throws Exception {
         String hmac = calculateHMAC("data", "key");
