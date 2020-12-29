1961114a3d6ede2c360e63b2a3da196bb944bfac
Manoj <manoj.khanwalkar@gmail.com>
2020-12-29 09:17:10 -0500

    Simple wrapper over jackson to serialize and deserialize a java object

---
+++ src/main/java/util/IdGenerator.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.security.SecureRandom;
+import java.util.UUID;
+
+public class IdGenerator {
+
+    static SecureRandom secureRandom = new SecureRandom();
+    public static int getId()
+    {
+        return secureRandom.nextInt();
+    }
+    public static int getId(int upperBound)
+    {
+        return secureRandom.nextInt(upperBound);
+    }
+    public static int getId(int lowerBound , int upperBound)
+    {
+        return lowerBound + secureRandom.nextInt(upperBound-lowerBound);
+    }
+
+    public static String getUniqueId()
+    {
+        return UUID.randomUUID().toString();
+    }
+
+}
